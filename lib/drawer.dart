9eb491f3be4f2f243efe885b2b0b5f4d416a6d33
OmarElmanzalawy <omarmanz200@gmail.com>
2021-09-15 23:48:22 +0200

    added live mode

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'main.dart';
-//import 'globals.dart' as globals;
+import 'package:fluttertoast/fluttertoast.dart';
+import 'API.dart' as API;
+import 'dart:convert';
+import 'globals.dart' as globals;
 
-class MainDrawer extends StatelessWidget {
+class MainDrawer extends StatefulWidget {
   final ValueChanged<String>? toCurrencyChanged;
   final ValueChanged<String>? fromCurrencyChanged;
   const MainDrawer({ Key? key ,this.toCurrencyChanged,this.fromCurrencyChanged}) : super(key: key);
+
+  @override
+  _MainDrawerState createState() => _MainDrawerState();
+}
+
+class _MainDrawerState extends State<MainDrawer> {
+  final fetchedSuccesfullySnackBar = SnackBar(
+      content: Text('Rates Fetched Succesfully',style: TextStyle(fontSize: 15),),
+      backgroundColor: Colors.green,
+      padding: EdgeInsets.symmetric(vertical: 12),
+      duration: Duration(seconds: 8),
+    );
+  final offlineModeSnackBar = SnackBar(
+      content: Text('Offline mode enabled',style: TextStyle(fontSize: 15),),
+      backgroundColor: Colors.red,
+      padding: EdgeInsets.symmetric(vertical: 12),
+      duration: Duration(seconds: 8),
+    );    
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
@@ -33,49 +53,49 @@ class MainDrawer extends StatelessWidget {
                 title: Text('EGP'),
                 subtitle: Text('Egyptian Pound'),
                 onTap: (){
-                  fromCurrencyChanged!('EGP');
+                  widget.fromCurrencyChanged!('EGP');
                 },
               ),
               ListTile(
                 title: Text('USD'),
                 subtitle: Text('American Dollar'),
                 onTap: (){
-                  fromCurrencyChanged!('USD');
+                  widget.fromCurrencyChanged!('USD');
                 },
               ),
               ListTile(
                 title: Text('EUR'),
                 subtitle: Text('European Dollar'),
                 onTap: (){
-                  fromCurrencyChanged!('EUR');
+                  widget.fromCurrencyChanged!('EUR');
                 },
               ),
               ListTile(
                 title: Text('KD'),
                 subtitle: Text('Kuwati Dinar'),
                 onTap: (){
-                  fromCurrencyChanged!('KD');
+                  widget.fromCurrencyChanged!('KD');
                 },
               ),
               ListTile(
                 title: Text('CHF'),
                 subtitle: Text('Swiss Franc'),
                 onTap: (){
-                  fromCurrencyChanged!('CHF');
+                  widget.fromCurrencyChanged!('CHF');
                 },
               ),
               ListTile(
                 title: Text('Q'),
                 subtitle: Text('Qatari Riyal'),
                 onTap: (){
-                  fromCurrencyChanged!('Q');
+                  widget.fromCurrencyChanged!('Q');
                 },
               ),
               ListTile(
                 title: Text('TURK'),
                 subtitle: Text('Turkish Lira'),
                 onTap: (){
-                  fromCurrencyChanged!('TURK');
+                  widget.fromCurrencyChanged!('TURK');
                 },
               ),                                                                    
             ],
@@ -89,55 +109,103 @@ class MainDrawer extends StatelessWidget {
                 title: Text('EGP'),
                 subtitle: Text('Egyptian Pound'),
                 onTap: (){
-                  toCurrencyChanged!('EGP');
+                  widget.toCurrencyChanged!('EGP');
                 },
               ),
               ListTile(
                 title: Text('USD'),
                 subtitle: Text('American Dollar'),
                 onTap: (){
-                  toCurrencyChanged!('USD');
+                  widget.toCurrencyChanged!('USD');
                 },
               ),
               ListTile(
                 title: Text('EUR'),
                 subtitle: Text('European Dollar'),
                 onTap: (){
-                  toCurrencyChanged!('EUR');
+                  widget.toCurrencyChanged!('EUR');
                 },                
               ),
               ListTile(
                 title: Text('KD'),
                 subtitle: Text('Kuwati Dinar'),
                 onTap: (){
-                  toCurrencyChanged!('KD');
+                  widget.toCurrencyChanged!('KD');
                 },                
               ),
               ListTile(
                 title: Text('CHF'),
                 subtitle: Text('Swiss Franc'),
                 onTap: (){
-                  toCurrencyChanged!('CHF');
+                  widget.toCurrencyChanged!('CHF');
                 },                
               ),
               ListTile(
                 title: Text('Q'),
                 subtitle: Text('Qatari Riyal'),
                 onTap: (){
-                  toCurrencyChanged!('Q');
+                  widget.toCurrencyChanged!('Q');
                 },                
               ),
               ListTile(
                 title: Text('TURK'),
                 subtitle: Text('Turkish Lira'),
                 onTap: (){
-                  toCurrencyChanged!('TURK');
+                  widget.toCurrencyChanged!('TURK');
                 },                
               ),                                                             
             ],
+          ),
+          SwitchListTile(
+            value: globals.online,
+            onChanged: (value) async{
+              setState(() {
+                globals.online = value;
+              });
+                if(globals.online==true){
+                try{
+                Fluttertoast.showToast(
+                  msg: 'Fetching Rates...',
+                  toastLength: Toast.LENGTH_LONG,
+                  fontSize: 14,
+                  backgroundColor: Colors.green,
+                );
+                var data = await API.getRates();
+                print('finished running script');
+                Map<String,dynamic> decodedData = jsonDecode(data);
+                globals.liveRates = decodedData;
+                globals.ratesFetchedSuccesfully = true;
+                print(globals.liveRates);
+                Fluttertoast.showToast(
+                  msg: 'Rates Fetched Succesfully',
+                  toastLength: Toast.LENGTH_LONG,
+                  fontSize: 14,
+                  backgroundColor: Colors.green,
+                );                
+                }on Exception{
+                  globals.online = false;
+                }
+                }
+                else{
+                  globals.ratesFetchedSuccesfully = false;
+                  globals.liveRates.clear();
+                Fluttertoast.showToast(
+                  msg: 'Offline Mode Enabled',
+                  toastLength: Toast.LENGTH_SHORT,
+                  fontSize: 14,
+                  backgroundColor: Colors.red,
+                );
+                }
+            },
+            title: Text('Live Rates'),
+            subtitle: Text('Gets Real-Time conversion rates'),
+            activeColor: Colors.green,
+            inactiveThumbColor: Colors.red.shade400,
+            inactiveTrackColor: Colors.red.shade200,
           ),
         ],
       ),
     );
   }
+
 }
